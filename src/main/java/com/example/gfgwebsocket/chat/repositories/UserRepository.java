19e88232359c395175b043a4038e5f2a906c47c3
RabakaRemo <mehzarin01781@gmail.com>
2024-12-25 04:07:43 +0600

    Updated

--- src/main/java/com/example/gfgwebsocket/chat/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gfgwebsocket.chat.repositories;
-
-import com.example.gfgwebsocket.chat.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
